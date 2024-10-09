a9f3bb45e36bab3f885cbe13ee1f23431af27a35
shailesh_tagadghar <shaileshtagadghar13@gmail.com>
2024-10-09 14:02:22 +0530

    .

--- lib/Modules/Home/Widget/product_cart_widget.dart
+++ lib/Modules/Home/Widget/product_cart_widget.dart
@@ -4,6 +4,7 @@ import 'package:fashion/Utils/Constants/color_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
+// ignore: must_be_immutable
 class ProductCartWidget extends StatefulWidget {
   final String? id;
   final String name;
