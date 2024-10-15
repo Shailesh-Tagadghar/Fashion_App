472297fb7ae86fadddc6f0e8d69a8e503e4f375e
shailesh_tagadghar <shaileshtagadghar13@gmail.com>
2024-10-15 14:51:35 +0530

    .

--- lib/Modules/Auth/services/fcm_service.dart
+++ lib/Modules/Auth/services/fcm_service.dart
@@ -1,25 +1,11 @@
-import 'dart:developer';
-
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 class FcmService {
-  // final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
-  // final GetStorage _storage = GetStorage();
   String? fcm_token = '';
 
   Future<void> initialize() async {
-    //   String? token = await _firebaseMessaging.getToken();
-
-    //   if (token != null) {
-    //     _storage.write('fcm_token', token);
-    //     log("FCM Token: $token");
-    //   } else {
-    //     log("Failed to get FCM Token");
-    //   }
-    // }
-
     try {
       fcm_token = kIsWeb
           ? 'Default_Fcm_Token'
@@ -29,13 +15,4 @@ class FcmService {
       fcm_token = 'Default_Fcm_Token';
     }
   }
-
-// try {
-//       fcm_token = kIsWeb
-//           ? 'Default_Fcm_Token'
-//           : await FirebaseMessaging.instance.getToken();
-//       GetStorage().write('fcm_token', fcm_token);
-//     } catch (e) {
-//       fcm_token = 'Default_Fcm_Token';
-//     }
 }
