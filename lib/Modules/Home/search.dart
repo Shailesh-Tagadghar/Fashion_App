bbe100fd1680f301984a564669aa81058b441571
shailesh_tagadghar <shaileshtagadghar13@gmail.com>
2024-10-17 21:41:43 +0530

    search screen is done with responsive

--- lib/Modules/Home/search.dart
+++ lib/Modules/Home/search.dart
@@ -1,9 +1,12 @@
 import 'package:fashion/Modules/Auth/Widget/custom_text.dart';
 import 'package:fashion/Modules/Auth/services/api_service.dart';
+import 'package:fashion/Modules/Home/Widget/product_cart_widget.dart';
 import 'package:fashion/Modules/Home/controllers/data_contoller.dart';
 import 'package:fashion/Routes/app_routes.dart';
 import 'package:fashion/Utils/Constants/api_constants.dart';
+import 'package:fashion/Utils/Constants/asset_constant.dart';
 import 'package:fashion/Utils/Constants/color_constant.dart';
+import 'package:fashion/Utils/Constants/responsive.dart';
 import 'package:fashion/Utils/Constants/string_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -25,42 +28,48 @@ class Search extends StatelessWidget {
         backgroundColor: ColorConstants.whiteColor,
         toolbarHeight: 10.h,
         leadingWidth: 15.w,
-        title: const CustomText(
-          text: StringConstants.search,
-          weight: FontWeight.w500,
-          fontSize: 13,
+        title: FittedBox(
+          child: CustomText(
+            text: StringConstants.search,
+            weight: FontWeight.w500,
+            fontSize: Responsive.isDesktop(context) ? 4 : 13,
+          ),
         ),
         centerTitle: true,
-        leading: Container(
-          alignment: Alignment.centerLeft,
-          margin: EdgeInsets.only(
-            left: 4.w,
-          ),
-          padding: EdgeInsets.all(
-            0.6.w,
-          ),
-          decoration: BoxDecoration(
-            border: Border.all(color: ColorConstants.lightGrayColor, width: 1),
-            shape: BoxShape.circle,
-          ),
-          child: IconButton(
-            iconSize: 24,
-            icon: const Icon(
-              Bootstrap.arrow_left,
-              color: ColorConstants.blackColor,
+        leading: FittedBox(
+          child: Container(
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(
+              left: Responsive.isDesktop(context) ? 0.2.w : 4.w,
             ),
-            onPressed: () {
-              Get.back();
-              // Navigator.pop(context);
-            },
+            padding: EdgeInsets.all(
+              Responsive.isDesktop(context) ? 0.2.w : 0.6.w,
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                  color: ColorConstants.lightGrayColor,
+                  width: Responsive.isDesktop(context) ? 0 : 1),
+              shape: BoxShape.circle,
+            ),
+            child: IconButton(
+              iconSize: Responsive.isDesktop(context) ? 20 : 24,
+              icon: const Icon(
+                Bootstrap.arrow_left,
+                color: ColorConstants.blackColor,
+              ),
+              onPressed: () {
+                Get.toNamed(AppRoutes.homewebScreen);
+              },
+            ),
           ),
         ),
       ),
       body: Padding(
         padding: EdgeInsets.only(
-          left: 5.w,
-          right: 5.w,
+          left: Responsive.isDesktop(context) ? 25.w : 5.w,
+          right: Responsive.isDesktop(context) ? 25.w : 5.w,
           bottom: 2.h,
+          top: Responsive.isDesktop(context) ? 4.h : 0,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -106,22 +115,34 @@ class Search extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const CustomText(
-                  text: StringConstants.recent,
-                  color: ColorConstants.blackColor,
-                  fontSize: 14,
-                  weight: FontWeight.w500,
+                FittedBox(
+                  child: CustomText(
+                    text: StringConstants.recent,
+                    color: ColorConstants.blackColor,
+                    fontSize: Responsive.isDesktop(context)
+                        ? 4
+                        : Responsive.isDesktop(context)
+                            ? 7
+                            : 14,
+                    weight: FontWeight.w500,
+                  ),
                 ),
                 GestureDetector(
                   onTap: () {
                     searchController.clear();
                     searchResults.clear();
                   },
-                  child: const CustomText(
-                    text: StringConstants.clear,
-                    color: ColorConstants.rich,
-                    fontSize: 14,
-                    weight: FontWeight.w500,
+                  child: FittedBox(
+                    child: CustomText(
+                      text: StringConstants.clear,
+                      color: ColorConstants.rich,
+                      fontSize: Responsive.isDesktop(context)
+                          ? 4
+                          : Responsive.isDesktop(context)
+                              ? 5
+                              : 14,
+                      weight: FontWeight.w500,
+                    ),
                   ),
                 ),
               ],
@@ -155,28 +176,61 @@ class Search extends StatelessWidget {
                       var product = searchResults[index];
                       return ListTile(
                         contentPadding: EdgeInsets.only(
-                          bottom: 1.h,
-                          left: 4.w,
-                          right: 2.w,
-                          top: 1.h,
+                          bottom: Responsive.isDesktop(context)
+                              ? 14.h
+                              : Responsive.isTablet(context)
+                                  ? 13.h
+                                  : 1.h,
+                          left: Responsive.isDesktop(context)
+                              ? 4.w
+                              : Responsive.isTablet(context)
+                                  ? 4.w
+                                  : 4.w,
+                          right: Responsive.isDesktop(context)
+                              ? 2.w
+                              : Responsive.isTablet(context)
+                                  ? 2.w
+                                  : 2.w,
+                          top: Responsive.isDesktop(context)
+                              ? 14.h
+                              : Responsive.isTablet(context)
+                                  ? 13.h
+                                  : 1.h,
                         ),
                         textColor: ColorConstants.blackColor,
-                        titleTextStyle: const TextStyle(
+                        titleTextStyle: TextStyle(
                           color: ColorConstants.blackColor,
-                          fontSize: 19,
+                          fontSize: Responsive.isDesktop(context)
+                              ? 24
+                              : Responsive.isTablet(context)
+                                  ? 20
+                                  : 19,
                           fontWeight: FontWeight.w400,
                         ),
-                        subtitleTextStyle: const TextStyle(
+                        subtitleTextStyle: TextStyle(
                           color: ColorConstants.blackColor,
-                          fontSize: 19,
+                          fontSize: Responsive.isTablet(context)
+                              ? 24
+                              : Responsive.isDesktop(context)
+                                  ? 20
+                                  : 19,
                           fontWeight: FontWeight.w400,
                         ),
                         tileColor: ColorConstants.lightGrayColor,
                         leading: Image.network(
-                          '${ApiConstants.imageBaseUrl}${product['image'][0]}', // Ensure this URL is valid
+                          '${ApiConstants.imageBaseUrl}${product['image'][0]}',
+                          // Ensure this URL is valid
                           fit: BoxFit.fill,
-                          height: 40.h,
-                          width: 25.w,
+                          height: Responsive.isDesktop(context)
+                              ? 10.h
+                              : Responsive.isTablet(context)
+                                  ? 30.h
+                                  : 40.h,
+                          width: Responsive.isDesktop(context)
+                              ? 10.w
+                              : Responsive.isTablet(context)
+                                  ? 30.w
+                                  : 25.w,
                         ),
                         title: Text(
                             'Name : ${product['name']}'), // Assuming 'name' is a field
@@ -185,20 +239,34 @@ class Search extends StatelessWidget {
 
                         onTap: () {
                           // Navigate to product details
-                          Get.toNamed(AppRoutes.productDetailsScreen,
-                              arguments: product);
+                          if (Responsive.isDesktop(context)) {
+                            Get.toNamed(AppRoutes.productDwebScreen,
+                                arguments: product);
+                          } else {
+                            Get.toNamed(AppRoutes.productDetailsScreen,
+                                arguments: product);
+                          }
                         },
                       );
                     },
                   ),
                   //     GridView.builder(
                   //   padding: EdgeInsets.zero,
-                  //   gridDelegate:
-                  //       const SliverGridDelegateWithFixedCrossAxisCount(
-                  //     crossAxisCount: 2, // Number of items per row
-                  //     childAspectRatio: 0.69, // Adjusts the size of the items
-                  //     crossAxisSpacing: 4.0, // Spacing between columns
-                  //     mainAxisSpacing: 4.0, // Spacing between rows
+                  //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  //     crossAxisCount: Responsive.isDesktop(context)
+                  //         ? 4
+                  //         : Responsive.isTablet(context)
+                  //             ? 3
+                  //             : 2,
+                  //     childAspectRatio: Responsive.isDesktop(context)
+                  //         ? 0.4
+                  //         : 0.69, // Adjusts the size of the items
+                  //     crossAxisSpacing: Responsive.isDesktop(context)
+                  //         ? 4
+                  //         : 4.0, // Spacing between columns
+                  //     mainAxisSpacing: Responsive.isDesktop(context)
+                  //         ? 4
+                  //         : 2.0, // Spacing between rows
                   //   ),
                   //   itemCount: searchResults.length,
                   //   shrinkWrap: true,
@@ -220,6 +288,8 @@ class Search extends StatelessWidget {
                   //             ? item['image']
                   //                 [0] // Use the first image from the list
                   //             : AssetConstant.pd3, // Fallback image
+                  //         isFavorite: false,
+                  //         onToggleFavorite: (p0) {},
                   //       ),
                   //     );
                   //   },
